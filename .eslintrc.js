d73bf8d0da2614efe6979093bad6e0db250775d1
davalapar <davalapar@gmail.com>
2019-12-02 12:59:54 +0800

    some progress

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+  },
+  "extends": [
+    "airbnb",
+  ],
+  "env": {
+      "es6": true,
+      "node": true,
+      "jest": true,
+  },
+  "rules": {
+    "max-len": 0,
+    "no-bitwise": 0,
+    "no-param-reassign": 0
+  }
+};
